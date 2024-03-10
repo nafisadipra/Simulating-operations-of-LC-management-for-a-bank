2498852e4a7a1d67a169d6283ff97bbc76457fd8
Dhoritri <ishrak.alam32@gmail.com>
2024-03-10 15:51:29 +0600

    Sign in sign up update

--- src/common/signIN/SignINFXMLController.java
+++ src/common/signIN/SignINFXMLController.java
@@ -4,13 +4,19 @@
  */
 package common.signIN;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -38,5 +44,30 @@ public class SignINFXMLController implements Initializable {
         userList.add(new User("muyeed", "123"));
         userList.add(new User("Alam", "456"));
     }    
+
+    @FXML
+    private void switchToSignUp(ActionEvent event) {
+        try {
+            // FXML Location
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/common/SignUP/SignUPFXML.fxml"));
+            Parent root = loader.load();
+            
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            
+            // Set application title
+            stage.setTitle("Sign Up");
+
+            // Set application icon
+            //Image icon = new Image(getClass().getResourceAsStream("resources/icon.png"));
+            //stage.getIcons().add(icon);
+            
+            // Show Time
+            stage.setScene(new Scene(root));
+            stage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
--- src/common/signUP/SignUPFXMLController.java
+++ src/common/signUP/SignUPFXMLController.java
@@ -4,12 +4,22 @@
  */
 package common.signUP;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 /**
  *
@@ -17,18 +27,56 @@ import javafx.scene.control.Label;
  */
 public class SignUPFXMLController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField signUpEmail;
+    @FXML
+    private TextField signUpName;
+    @FXML
+    private DatePicker dateOfBirth;
+    @FXML
+    private TextField nationalID;
+    @FXML
+    private TextField phoneNumber;
+    @FXML
+    private PasswordField signUpPass;
+    @FXML
+    private Label switchToSignIn;
     
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void signInButtonAction(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void switchToLogIn(ActionEvent event) {
+        try {
+            // FXML Location
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/common/SignIN/SignINFXML.fxml"));
+            Parent root = loader.load();
+            
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            
+            // Set application title
+            stage.setTitle("Sign In");
+
+            // Set application icon
+            //Image icon = new Image(getClass().getResourceAsStream("resources/icon.png"));
+            //stage.getIcons().add(icon);
+            
+            // Show Time
+            stage.setScene(new Scene(root));
+            stage.show();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }
