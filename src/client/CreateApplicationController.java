ff9b6af8e6041df201dce0d7450e1e33f1a4cae1
Nafisa420 <nafisaanzumdipra@gmail.com>
2024-04-12 17:41:15 +0600

    Features Added

--- src/client/CreateApplicationController.java
+++ src/client/CreateApplicationController.java
@@ -540,7 +540,7 @@ public class CreateApplicationController implements Initializable {
             proData += "\n" + X.getProduct()+ "▓" + X.getPrice() + "▓"+ X.getQuantity()+ "▓" + X.getAmount()+ "▓" ;
             
         }
-        String alldata= cliData + merData + DateAndTime + amount +proData;
+        String alldata= cliData + merData + DateAndTime + amount +"\nPending" + proData;
         new Writer("Database/Official/PI",  piNum + ".bin", alldata).writeFile();
         new Writer("Database/User/CLIENT/"+ email,"pi.bin", Long.toString(piNum)).overWriteFile();
         
