98407062893ea877f63172270fbff2e9cf1ef14b
Nafisa420 <nafisaanzumdipra@gmail.com>
2024-04-14 14:14:15 +0600

    Features Added

--- src/client/SceneView.java
+++ src/client/SceneView.java
@@ -15,10 +15,10 @@ public class SceneView extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateTransaction.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Application.fxml"));
         Parent root = loader.load();
         
-        client.CreateTransactionController controller = loader.getController();
+        client.ApplicationController controller = loader.getController();
         String[] sanData = {"Dashboard", "Contact", "Notification", "Application", "Transaction", "Invoice", "Switch Account", "Settings", "Policy", "Feedback"};
         controller.initData("CLIENT", "dipra@lc.cli.com", sanData);
         
